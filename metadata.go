a3e1fc67bc80bdb58fcb0421d6f9994710a00e82
Rey David <rdominguez@tecnologer.net>
2023-02-09 21:00:57 -0600

    medatada: add method string

--- metadata.go
+++ metadata.go
@@ -1,7 +1,20 @@
 package engine
 
+import "fmt"
+
 type Metadata interface {
 	GetName() string
 	GetMaxPlayer() int
 	GetMinPlayer() int
+	GetVersion() string
+	String() string
+}
+
+func MetadataToString(m Metadata) string {
+	return fmt.Sprintf("%s [players: %d-%d] (version: %s)",
+		m.GetName(),
+		m.GetMinPlayer(),
+		m.GetMaxPlayer(),
+		m.GetVersion(),
+	)
 }
