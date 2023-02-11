22d27981f7b5fa7ee2a7050d49484c655f9fe954
Rey David <rdominguez@tecnologer.net>
2023-02-10 21:55:12 -0600

    metadata: add comments to functions

--- metadata.go
+++ metadata.go
@@ -3,10 +3,15 @@ package engine
 import "fmt"
 
 type Metadata interface {
+	//GetName returns the name of the game version
 	GetName() string
+	//GetMaxPlayer returns an integer that represents the maximum of players to play
 	GetMaxPlayer() int
+	//GetMinPlayer returns an integer that represents the minimum amount of players to start a game
 	GetMinPlayer() int
+	//GetVersion returns the build version of the plugin
 	GetVersion() string
+	//String returns a representative's string of the metadata
 	String() string
 }
 
