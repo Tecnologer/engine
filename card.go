2ec26c30a04a5740e59b9745e012516ed37b43df
tecnologer <rdominguez@tecnologer.net>
2023-02-15 21:09:50 -0600

    card: update response to CardResult

--- card.go
+++ card.go
@@ -3,6 +3,6 @@ package engine
 type Card interface {
 	GetValue() string
 	GetColor() string
-	Play(string, []Card) (Result, error)
+	Play(string, []Card) (CardResult, error)
 	CanPlay(string, []Card) (bool, error)
 }
